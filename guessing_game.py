5990c2a126409f539cda83fdc08d249e968dde2e
Dylan Cowling <dylanjcowling@gmail.com>
2018-11-28 16:14:18 +0000

    * If player inputs number outside of range, they are prompted to try again

--- guessing_game.py
+++ guessing_game.py
@@ -3,7 +3,6 @@ import random
 
 # TODO: Create separate "def" for highscore and put the main game within that as to save highscores between games.
 # TODO: Make sure your script runs without errors. Catch exceptions and report errors to the user in a meaningful way.
-# TODO: Make sure the player can try again if they guess outside of the number range.
 
 
 def start_game():
@@ -22,18 +21,23 @@ def start_game():
     guesses.append(player_guess)
     # 3. Continuously prompt the player for a guess.
     while player_guess != random_num:
+        # Make sure the player can try again if they guess outside of the number range.
+        if player_guess > 25 or player_guess < 1:
+            guesses.append(player_guess)
+            player_guess = int(input("The number you guessed is not within 1 and 25, give it another shot:  "))
         #   If the guess greater than the solution, display to the player "It's lower".
-        if player_guess < random_num:
-            print("Too low!")
-        #   If the guess is less than the solution, display to the player "It's higher".
-        elif player_guess > random_num:
-            print("Too high!")
-        player_guess = int(input("Please try again:  "))
-        guesses.append(player_guess)
+        else:
+            if player_guess < random_num:
+                print("Too low!")
+            #   If the guess is less than the solution, display to the player "It's higher".
+            elif player_guess > random_num:
+                print("Too high!")
+            player_guess = int(input("Please try again:  "))
+            guesses.append(player_guess)
 
     # Once the guess is correct, stop looping, inform the user they "Got it"
     # and show how many attempts it took them to get the correct number.
-    print("\nCongratulations! You won in {} guesses.".format(len(guesses)))
+    print("\nCongratulations! You got it in {} guesses.".format(len(guesses)))
     # Let the player know the game is ending, or something that indicates the game is over.
     play_again = input("\nGame Over. Would you like to play again? (Y/N)  ").lower()
     if play_again == "y":
