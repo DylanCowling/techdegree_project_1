0f7ca628103c82d6d5b44627883ff9562ee7bd87
Dylan Cowling <dylanjcowling@gmail.com>
2018-11-28 14:53:45 +0000

    * Formatting

--- guessing_game.py
+++ guessing_game.py
@@ -7,14 +7,14 @@ import random
 
 
 def start_game():
-    # 1. Display an intro/welcome message to the player.
+    # Display an intro/welcome message to the player.
     print("""
     ._______________________.
     |_____Welcome to my_____|
     |_Number Guessing Game!_|
     |_______________________|""")
 
-    # 2. Store a random number as the answer/solution.
+    # Store a random number as the answer/solution.
     random_num = random.randint(1, 25)
     guesses = []
 
@@ -22,19 +22,19 @@ def start_game():
     guesses.append(player_guess)
     # 3. Continuously prompt the player for a guess.
     while player_guess != random_num:
-        #   a. If the guess greater than the solution, display to the player "It's lower".
+        #   If the guess greater than the solution, display to the player "It's lower".
         if player_guess < random_num:
             print("Too low!")
-        #   b. If the guess is less than the solution, display to the player "It's higher".
+        #   If the guess is less than the solution, display to the player "It's higher".
         elif player_guess > random_num:
             print("Too high!")
         player_guess = int(input("Please try again:  "))
         guesses.append(player_guess)
 
-    # 4. Once the guess is correct, stop looping, inform the user they "Got it"
-    #    and show how many attempts it took them to get the correct number.
+    # Once the guess is correct, stop looping, inform the user they "Got it"
+    # and show how many attempts it took them to get the correct number.
     print("\nCongratulations! You won in {} guesses.".format(len(guesses)))
-    # 5. Let the player know the game is ending, or something that indicates the game is over.
+    # Let the player know the game is ending, or something that indicates the game is over.
     play_again = input("\nGame Over. Would you like to play again? (Y/N)  ").lower()
     if play_again == "y":
         start_game()
